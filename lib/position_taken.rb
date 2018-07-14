5bd262b383158893bf253edccc2c875b9a8ebf76
Eli Cho <eli.cho@gmail.com>
2018-07-14 22:58:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] != " " || board[index] != "" || board[index] != nil
     return FALSE
-  else
+  else board[index] == "X" || board[index] == "O"
     return true
   end
 end
